ff4323f4eeaf19dc318aa0894d6dcc6864cb89d5
jesperboberg <60383436+jesperboberg@users.noreply.github.com>
2020-11-20 09:34:11 +0100

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -3,11 +3,22 @@ Created on 20 Nov 2020
 
 @author: jesperboberg
 '''
-
-
+import numpy as np
+import random
 
 
 def createRandomForest(size,density):
-    forest = 0
-    
+    forest = np.zeros(shape = (size,size)) 
+    amountTrees = np.floor(size*size*density)
+    amountTrees = int(amountTrees)
+    count = 0
+    while count < amountTrees:
+        randX = random.randint(0,size-1)
+        randY = random.randint(0,size-1)
+        if(forest[randX,randY] == 0):
+            forest[randX,randY] = 1
+            count += 1
     return forest
+
+
+    
