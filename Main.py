f65267e7a7079251ca068d0f87afe27acc41edb7
jesperboberg <60383436+jesperboberg@users.noreply.github.com>
2020-11-24 23:42:39 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,17 +3,47 @@ Created on 20 Nov 2020
 
 @author: jesperboberg
 '''
-from .Parameters import parameters
-from .Predator import Predator
-from .Prey import Prey
-from .Functions import createRandomForest
 
+from Basic_model.Parameters import parameters
+from Basic_model.Functions import createLattice,moveRandom,createRandomPrey,createRandomPredator,updateLattice
+import matplotlib.pyplot as plt
+import numpy as np 
 
 def main():
     paraDict = parameters()
-    p1 = paraDict['para1']
-    print(paraDict['para3'])
-    print(p1)
+    # Plot setup and initializing random populations
+    plt.ion()
+    fig, ax = plt.subplots()
+    preyPopulationForest = createRandomPrey(paraDict)
+    x = [prey[0] for prey in preyPopulationForest]
+    y = [prey[1] for prey in preyPopulationForest]
+    sc1 = ax.scatter(x,y,color = 'blue', s = 1)
+    predatorPopulationForest = createRandomPredator(paraDict)
+    x = [predator[0] for predator in predatorPopulationForest]
+    y = [predator[1] for predator in predatorPopulationForest]
+    sc2 = ax.scatter(x,y,color = 'red', s = 1)
+    forest = createLattice(paraDict['forestSize'],preyPopulationForest,predatorPopulationForest)
+    plt.xlim(0,paraDict['forestSize'])
+    plt.ylim(0,paraDict['forestSize'])
+    plt.title('Forest with predators and prey')
+    plt.draw()
+    # Make a random movement for every prey/predator and update plots live
+    for t in range(paraDict['timeSteps']):
+        print(t)
+        for pred in predatorPopulationForest[:]:
+            forest, predatorPopulationForest = moveRandom(pred,predatorPopulationForest,forest)
 
+        for prey in preyPopulationForest[:]:
+            forest, preyPopulationForest = moveRandom(prey,preyPopulationForest,forest)
+
+        forest,preyPopulationForest = updateLattice(forest[:],preyPopulationForest[:])
+        x = [prey[0] for prey in preyPopulationForest]
+        y = [prey[1] for prey in preyPopulationForest]
+        sc1.set_offsets(np.c_[x,y])
+        x = [predator[0] for predator in predatorPopulationForest]
+        y = [predator[1] for predator in predatorPopulationForest]
+        sc2.set_offsets(np.c_[x,y])
+        fig.canvas.draw_idle()
+        plt.pause(0.2) 
 
 main()
