80d6148315d32ecfb1b1eb29f904a0b43f332f3f
samberngit <sambern@student.chalmers.se>
2020-11-26 17:03:25 +0100

    Testing

--- Parameters.py
+++ Parameters.py
@@ -7,7 +7,7 @@ import numpy as np
 
 def parameters():
     forestSize = 100 # Right now N*N, could be made into M*N
-    preyPopulationSize = 50
+    preyPopulationSize = 51
     predatorPopulationSize = 5
     timeSteps = 100
     return locals()
